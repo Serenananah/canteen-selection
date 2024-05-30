2f2e8338b08d3faf48cd180b4194d3880b743190
Serenananah <1798831336@qq.com>
2024-05-30 21:37:51 +0800

    try

--- backend/src/DBConnection.cpp
+++ backend/src/DBConnection.cpp
@@ -1,10 +1,10 @@
 // backend/src/DBConnection.cpp
-#include "DBConnection.h"
+#include "../include/DBConnection.h"
 #include <cppconn/exception.h>
 
 DBConnection::DBConnection() {
     driver = sql::mysql::get_mysql_driver_instance();
-    conn = driver->connect("tcp://127.0.0.1:3306", "root", "Angel20050407#"); // 替换为您的数据库用户名和密码
+    conn = driver->connect("tcp://127.0.0.1:3306", "root", "Angel20050407#"); 
     conn->setSchema("canteen_system");
 }
 
